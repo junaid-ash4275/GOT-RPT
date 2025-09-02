cf571234b325e1cb3b9eb52c7ffd75f18c9dcaac
Junaidashrafgithub <junaid.ashraf4275@gmail.com>
2025-09-02 11:44:51 +0500

    chore: fix typo in App.js comment

--- src/App.js
+++ src/App.js
@@ -54,7 +54,7 @@ import "assets/css/nucleo-icons.css";
 import "assets/css/nucleo-svg.css";
 
 export default function App() {
-  //Created new branch named develop
+  //modifiede this comment
   const isUserLoggedIn = store.getState().user.isUserLoggedIn;
   const [controller, dispatch] = useArgonController();
   const { miniSidenav, direction, layout, openConfigurator, sidenavColor, darkSidenav, darkMode } =
